efa1b9e1325bc58aaf2367a433d6b6bb97505a2c
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-07-19 09:55:18 -0300

    feat: creating users list and create an new user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,51 @@
+import { InferModel } from 'drizzle-orm'
+
 import { db } from '@/db'
 import { users } from '@/db/schema'
+import { revalidatePath } from 'next/cache'
+
+type NewUser = InferModel<typeof users, 'insert'>
 
 export default async function Home() {
   const allUsers = await db.select().from(users)
 
-  return <p>{JSON.stringify(allUsers, null, 2)}</p>
+  async function addUser(data: FormData) {
+    'use server'
+
+    const fullName = data.get('full_name')?.toString()
+    const phone = data.get('phone')?.toString()
+
+    if (!fullName || !phone) {
+      return
+    }
+
+    await db.insert(users).values({
+      fullName,
+      phone,
+    })
+
+    revalidatePath('/')
+  }
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center gap-6 bg-zinc-900 text-zinc-50">
+      <p>{JSON.stringify(allUsers, null, 2)}</p>
+      <form action={addUser} className="flex flex-col gap-3">
+        <input
+          type="text"
+          name="full_name"
+          placeholder="Full name"
+          className="bg-zinc-900"
+        />
+        <input
+          type="text"
+          name="phone"
+          placeholder="Phone"
+          className="bg-zinc-900"
+        />
+
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  )
 }
